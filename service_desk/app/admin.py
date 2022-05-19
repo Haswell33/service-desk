85985dfb4be20f860dcccfcbed72e4b791a68276
haswell33 <uY1pX0el1xWolAbGqjW7>
2022-05-19 03:11:15 +0200

    building templates 9.1

--- service_desk/app/admin.py
+++ service_desk/app/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.contrib.admin import AdminSite
 from django.contrib.auth.models import Group
-
 #from django.contrib.sites.models import Site
+
 from app.models import Issue, Tenant, Comment, Priority, Status, Resolution, Type, Label, SLA, Workflow
 
+AdminSite.index_title = 'Administration'
 admin.site.register(Issue)
 admin.site.register(Status)
 admin.site.register(Type)
@@ -14,7 +15,6 @@ admin.site.register(SLA)
 admin.site.register(Workflow)
 admin.site.register(Priority)
 admin.site.register(Tenant)
-admin.site.register
 admin.site.site_title = 'nowy title'
 admin.site.site_header = 'nowy header'
 # admin.site.unregister(Group)
