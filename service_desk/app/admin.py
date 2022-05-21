21e60dd9e97cdead7eedcd961734cb392a8c5ca1
haswell33 <uY1pX0el1xWolAbGqjW7>
2022-05-21 02:11:06 +0200

    building templates 10

--- service_desk/app/admin.py
+++ service_desk/app/admin.py
@@ -1,13 +1,31 @@
 from django.contrib import admin
+from django import forms
 from django.contrib.admin import AdminSite
 from django.contrib.auth.models import Group
 #from django.contrib.sites.models import Site
 
 from app.models import Issue, Tenant, Comment, Priority, Status, Resolution, Type, Label, SLA, Workflow
 
+class StatusAdminForm():
+    pass
+
+class StatusAdminModel(admin.ModelAdmin):
+    class Meta:
+        #model = Status
+        #fields = (('id', 'name', 'type'), 'description')
+        fieldsets = (
+            (None, {
+                'fields': ('id', 'name')
+            }),
+            ('Advanced options', {
+                'classes': ('collapse',),
+                'fields': ('description', 'type'),
+            }),
+        )
+
 AdminSite.index_title = 'Administration'
 admin.site.register(Issue)
-admin.site.register(Status)
+admin.site.register(Status, StatusAdminModel)
 admin.site.register(Type)
 admin.site.register(Resolution)
 admin.site.register(Label)
