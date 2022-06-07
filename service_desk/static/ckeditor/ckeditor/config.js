02b7fa7f8c1eaadcbe822e559c4edc82df6e1f44
haswell33 <uY1pX0el1xWolAbGqjW7>
2022-06-07 02:48:01 +0200

    building issue form 3

---
+++ service_desk/static/ckeditor/ckeditor/config.js
@@ -0,0 +1,12 @@
+/**
+ * @license Copyright (c) 2003-2022, CKSource Holding sp. z o.o. All rights reserved.
+ * For licensing, see https://ckeditor.com/legal/ckeditor-oss-license
+ */
+
+CKEDITOR.editorConfig = function( config ) {
+	// Define changes to default configuration here. For example:
+	// config.language = 'fr';
+	config.uiColor = '#656565';
+    config.colorButton_colors = 'CF5D4E,454545,FFF,DDD,CCEAEE,66AB16';
+    config.skin = 'kama';
+};
